cccccc8250319e536020403f4231611b585e16d1
AhnEunJin <eunjin.an@datastreams.co.kr>
2024-06-20 12:13:04 +0900

    연락처 추가하기

--- src/component/ContactForm.jsx
+++ src/component/ContactForm.jsx
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
 
 export default function ContactForm() {
+    const [contact, setContact] = useState({ name: '', phoneNumber: '' });
+    const dispatch = useDispatch();
     return (
-        <Form>
+        <Form
+            onSubmit={(e) => {
+                e.preventDefault();
+                dispatch({
+                    type: 'ADD_CONTACT',
+                    payload: {
+                        name: contact.name,
+                        phoneNumber: contact.phoneNumber,
+                    },
+                });
+            }}
+        >
             <Form.Group className='mb-3' controlId='formName'>
                 <Form.Label>이름</Form.Label>
-                <Form.Control type='text' placeholder='이름을 입력해주세요' />
+                <Form.Control
+                    type='text'
+                    placeholder='이름을 입력해주세요'
+                    onChange={(e) => {
+                        setContact({ ...contact, name: e.target.value });
+                        console.log(contact);
+                    }}
+                />
             </Form.Group>
-            <Form.Group className='mb-3' controlId='formContactNum'>
+            <Form.Group className='mb-3' controlId='formPhoneNum'>
                 <Form.Label>전화번호</Form.Label>
                 <Form.Control
                     type='number'
                     placeholder='전화번호를 입력해주세요'
+                    onChange={(e) => {
+                        setContact({ ...contact, phoneNumber: e.target.value });
+                        console.log(contact);
+                    }}
                 />
             </Form.Group>
             <Button variant='primary' type='submit'>
--- src/component/ContactList.jsx
+++ src/component/ContactList.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import SearchBox from './SearchBox';
 import ContactItem from './ContactItem';
+import { useSelector } from 'react-redux';
 
 export default function ContactList() {
+    const contactList = useSelector((state) => state.contactList);
     return (
         <div style={{ marginTop: 32 }}>
             <SearchBox />
-            <ContactItem />
+            {contactList.map((contact) => (
+                <ContactItem />
+            ))}
         </div>
     );
 }
